58c3677e226954d559717beb5318433c04a13c31
Zana Attila <atti.zana@gmail.com>
2022-10-22 15:07:50 +0200

    Project creation and dotnet version update

---
+++ ZCSVParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZCSVParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
