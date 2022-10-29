47c4d9be6af3a990ad6400caad6cdf68c8cd932a
Zana Attila <atti.zana@gmail.com>
2022-10-29 09:43:26 +0200

    Validating skeleton

---
+++ ZCSVParser/Validator/IValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZCSVParser.Validator
+{
+    internal interface IValidator
+    {
+        bool Validate(string validatable);
+        bool Validate(IEnumerable<string> validatables);
+        bool Validate(string[] validatables);
+    }
+}
---
+++ ZCSVParser/Validator/PathValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZCSVParser.Validator
+{
+    public sealed class PathValidator : IValidator
+    {
+        public bool Validate(string validatable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(IEnumerable<string> validatables)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(string[] validatables)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
